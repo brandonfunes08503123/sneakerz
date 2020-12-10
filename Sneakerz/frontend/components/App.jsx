e6cea947d3339e0eec8394ec47080c9e4899c33b
FrontendMasters <ctanjoco@gmail.com>
2020-12-09 22:29:33 -0800

    Added Navbar component

--- Sneakerz/frontend/components/App.jsx
+++ Sneakerz/frontend/components/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from './navbar/navbar_container'
 
 const App = () =>  (
-    null;
+    <div className="app-container">
+        <header>
+            <h1>It's alive!</h1>
+            <Navbar />
+        </header>
+    </div>
 )
 
-export default App;
+export default 
