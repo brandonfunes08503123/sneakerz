7296f746f9e9d1371f7b3a12bbc1e54886645aa5
FrontendMasters <ctanjoco@gmail.com>
2020-12-18 09:38:08 -0800

    Resolving conflicts

--- frontend/reducers/entities_Reducer.js
+++ frontend/reducers/entities_Reducer.js
@@ -5,15 +5,10 @@
 
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
-<<<<<<< HEAD
-
-const entitiesReducer = combineReducers({
-=======
 import sneakersReducer from "./sneakers_reducer";
 
 const entitiesReducer = combineReducers({
   sneakers: sneakersReducer,
->>>>>>> search_products
   users: usersReducer,
 });
 
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,17 +1,10 @@
 import { combineReducers } from "redux";
-<<<<<<< HEAD
-import ModalReducer from "./modal_reducer";
-
-const uiReducer = combineReducers({
-  modal: ModalReducer,
-=======
 import searchModalReducer from "./search_modal_reducer";
 import menuModalReducer from "./menu_modal_reducer";
 
 const uiReducer = combineReducers({
   search: searchModalReducer,
   menu: menuModalReducer,
->>>>>>> search_products
 });
 
 export default uiReducer;
